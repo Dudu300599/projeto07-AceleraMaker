be279e9479f63d2b87fea3fe94fd3f56453a6069
Carlos Eduardo <79771964+Dudu300599@users.noreply.github.com>
2026-06-27 13:11:59 -0300

    Update CLIPGM.cbl

--- src/CLIPGM.cbl
+++ src/CLIPGM.cbl
@@ -1,95 +1,141 @@
-IDENTIFICATION DIVISION.
-       PROGRAM-ID. CLIPGM.
-       
-       ENVIRONMENT DIVISION.
-       
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01  WS-COMMAREA          PIC X(01).
-       01  WS-RESP              PIC S9(8) COMP.
-       
-       01  REG-CLIENTE.
-           05 REG-CODCLI        PIC 9(06).
-           05 REG-NOME          PIC X(30).
-           05 REG-TELEFONE      PIC X(15).
-           05 REG-CIDADE        PIC X(20).
+       IDENTIFICATION DIVISION.                            
+       PROGRAM-ID. CLIPGM.                                 
+                                                           
+       ENVIRONMENT DIVISION.                               
+                                                           
+       DATA DIVISION.                                      
+       WORKING-STORAGE SECTION.                            
+                                                           
+       01  WS-CLIENTE-REG.                                 
+           05 WS-CODCLI     PIC X(06).                     
+           05 WS-NOME       PIC X(30).                     
+           05 WS-TELEFONE   PIC X(15).                     
+           05 WS-CIDADE     PIC X(20).                     
+                                                           
+       01  WS-COMMAREA      PIC X(06).                     
+       01  WS-RESP          PIC S9(8) COMP.                
+       01  WS-MSG-ERRO.                                    
+           05 FILLER      PIC X(11) VALUE 'ERRO VSAM: '.   
+           05 WS-COD-ERRO PIC 9(08).                                                                           
+        COPY MAPCLIE.                                                    
+        COPY DFHAID.                                                     
+                                                                         
+        LINKAGE SECTION.                                                 
+        01  DFHCOMMAREA      PIC X(06).                                  
+                                                                         
+        PROCEDURE DIVISION.                                              
+        000-MAIN.                                                        
+            IF EIBCALEN = 0                                              
+               PERFORM 100-INICIALIZA-TELA                               
+            ELSE                                                         
+               PERFORM 200-PROCESSA-TELA.                                
+                                                                         
+            EXEC CICS RETURN END-EXEC.                                   
+                                                                         
+        100-INICIALIZA-TELA.                                             
+            MOVE LOW-VALUES TO TELACLIO.                                 
+            EXEC CICS SEND                                               
+                     MAP('TELACLI')                                     
+                     MAPSET('MAPCLIE')                                  
+                     ERASE                                              
+           END-EXEC.                                                    
+                                                                        
+           EXEC CICS RETURN                                             
+                     TRANSID('CLIE')                                    
+                     COMMAREA(WS-COMMAREA)                              
+                     LENGTH(6)                                          
+           END-EXEC.                                                    
+                                                                        
+       200-PROCESSA-TELA.                                               
+           EXEC CICS RECEIVE                                            
+                     MAP('TELACLI')                                     
+                     MAPSET('MAPCLIE')                                  
+                     RESP(WS-RESP)                                      
+           END-EXEC.                                                    
+                                                                        
+           IF EIBAID = DFHPF3                                           
+              EXEC CICS RETURN END-EXEC.                                
+           IF EIBAID = DFHPF5
+              PERFORM 300-CONSULTAR.                                
+           IF EIBAID = DFHPF6                                       
+              PERFORM 400-SALVAR.                                   
+                                                                    
+           IF EIBAID NOT = DFHPF3 AND EIBAID NOT = DFHPF5           
+              AND EIBAID NOT = DFHPF6                               
+              MOVE 'TECLA INVALIDA.' TO MENSAGMO                    
+              PERFORM 900-ENVIA-TELA.                               
+       300-CONSULTAR.                                               
+                                                                    
+           MOVE CODCLII TO WS-CODCLI.                               
+           MOVE CODCLII TO WS-COMMAREA.                             
+           EXEC CICS READ                                           
+                     DATASET('CLIENTES')                            
+                     INTO(WS-CLIENTE-REG)                           
+                     RIDFLD(WS-CODCLI)                              
+                     RESP(WS-RESP)                                  
+           END-EXEC.                                                
+                                                               
+           IF WS-RESP = DFHRESP(NORMAL)                       
+              MOVE WS-NOME     TO NOMEO                       
+              MOVE WS-TELEFONE TO TELEFONO                    
+              MOVE WS-CIDADE   TO CIDADEO                     
+              MOVE 'CLIENTE ENCONTRADO.' TO MENSAGMO          
+           ELSE                                               
+              MOVE WS-RESP TO WS-COD-ERRO                     
+              MOVE WS-MSG-ERRO TO MENSAGMO.                   
+           PERFORM 900-ENVIA-TELA.                            
+                                                              
+       400-SALVAR.                                            
+                                                              
+           MOVE KIKCOMMAREA TO WS-CODCLI.                     
+           MOVE KIKCOMMAREA TO WS-COMMAREA.                   
+           EXEC CICS READ                                     
+                     DATASET('CLIENTES')                      
+                     INTO(WS-CLIENTE-REG)                     
+                     RIDFLD(WS-CODCLI)                        
+                     UPDATE                                   
+                     RESP(WS-RESP)
+           END-EXEC.                                                    
+                                                                        
+           IF WS-RESP = DFHRESP(NORMAL)                                 
+              PERFORM 450-GRAVAR                                        
+           ELSE                                                         
+              MOVE 'CLIENTE NAO ENCONTRADO P/ ATUALIZAR.' TO MENSAGMO.  
+           PERFORM 900-ENVIA-TELA.                                      
+                                                                        
+       450-GRAVAR.                                                      
+           IF NOMEI NOT = LOW-VALUES                                    
+           IF NOMEI NOT = SPACES                                        
+              MOVE NOMEI TO WS-NOME.                                    
+           IF TELEFONI NOT = LOW-VALUES                                 
+           IF TELEFONI NOT = SPACES                                     
+              MOVE TELEFONI TO WS-TELEFONE.                             
+           IF CIDADEI NOT = LOW-VALUES                                  
+           IF CIDADEI NOT = SPACES                                      
+              MOVE CIDADEI TO WS-CIDADE.                                
+                                                                        
+           EXEC CICS REWRITE
+                     DATASET('CLIENTES')                                
+                     FROM(WS-CLIENTE-REG)                               
+                     RESP(WS-RESP)                                      
+           END-EXEC.                                                    
+           IF WS-RESP = DFHRESP(NORMAL)                                 
+              MOVE 'ALTERACAO REALIZADA COM SUCESSO.' TO MENSAGMO       
+           ELSE                                                         
+              MOVE 'ERRO AO GRAVAR.' TO MENSAGMO.                       
+                                                                        
+       900-ENVIA-TELA.                                                  
+           EXEC CICS SEND                                               
+                     MAP('TELACLI')                                     
+                     MAPSET('MAPCLIE')                                  
+                     DATAONLY                                           
+           END-EXEC.                                                    
+                                                                        
+           EXEC CICS RETURN                                             
+                     TRANSID('CLIE')                                    
+                     COMMAREA(WS-COMMAREA)
+                     LENGTH(6)                                         
+           END-EXEC.                                                   
+                                                                       
 
-       COPY MAPCLIE. 
-
-       LINKAGE SECTION.
-       01  DFHCOMMAREA          PIC X(01).
-
-       PROCEDURE DIVISION.
-       MAIN-LOGIC.
-           IF EIBCALEN = 0
-               PERFORM MANDA-TELA-VAZIA
-           ELSE
-               PERFORM RECEBE-TELA
-               PERFORM AVALIA-TECLA.
-           
-           * O GOBACK garante que o programa encerre caso fuja do fluxo
-           GOBACK.
-
-       MANDA-TELA-VAZIA.
-           EXEC CICS SEND 
-                MAP('TELA1') MAPSET('MAPCLIE') ERASE
-           END-EXEC.
-           EXEC CICS RETURN 
-                TRANSID('CLIE') COMMAREA(WS-COMMAREA) LENGTH(1)
-           END-EXEC.
-
-       RECEBE-TELA.
-           EXEC CICS RECEIVE 
-                MAP('TELA1') MAPSET('MAPCLIE')
-           END-EXEC.
-
-       AVALIA-TECLA.
-           IF EIBAID = DFHPF3
-               EXEC CICS RETURN END-EXEC
-           ELSE
-               IF EIBAID = DFHPF5
-                   PERFORM FLUXO-CONSULTA
-               ELSE
-                   IF EIBAID = DFHPF6
-                       PERFORM FLUXO-SALVAR
-                   ELSE
-                       MOVE 'TECLA INVALIDA.' TO MENSAGO
-                       PERFORM REENVIA-TELA.
-
-       FLUXO-CONSULTA.
-           MOVE CODCLII TO REG-CODCLI.
-           EXEC CICS READ 
-                DATASET('CLIENTES')
-                INTO(REG-CLIENTE)
-                RIDFLD(REG-CODCLI)
-                RESP(WS-RESP)
-           END-EXEC.
-           
-           IF WS-RESP = DFHRESP(NORMAL)
-               MOVE REG-NOME TO NOMEO
-               MOVE REG-TELEFONE TO TELEFO
-               MOVE REG-CIDADE TO CIDADEO
-               MOVE 'CLIENTE ENCONTRADO.' TO MENSAGO
-           ELSE
-               MOVE 'CLIENTE NAO ENCONTRADO.' TO MENSAGO.
-           
-           PERFORM REENVIA-TELA.
-
-       FLUXO-SALVAR.
-           MOVE TELEFI TO REG-TELEFONE.
-           MOVE CIDADEI TO REG-CIDADE.
-           EXEC CICS REWRITE 
-                DATASET('CLIENTES')
-                FROM(REG-CLIENTE)
-           END-EXEC.
-           
-           MOVE 'ALTERACAO REALIZADA.' TO MENSAGO.
-           PERFORM REENVIA-TELA.
-
-       REENVIA-TELA.
-           EXEC CICS SEND 
-                MAP('TELA1') MAPSET('MAPCLIE')
-           END-EXEC.
-           EXEC CICS RETURN 
-                TRANSID('CLIE') COMMAREA(WS-COMMAREA) LENGTH(1)
-           END-EXEC.
+                                                           
